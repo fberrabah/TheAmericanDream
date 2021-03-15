450652ca859febcd44e2cac8ce5a7a54d34b083e
fberrabah <farid.berrabah@gmail.com>
2021-03-15 12:21:52 +0100

    add files and read in pandas

---
+++ src/d01_data/load_data.py
@@ -0,0 +1,10 @@
+import csv
+import pandas as pd
+
+data = pd.read_csv('~/Desktop/AmericanDream/Data/01_raw/DataAnalyst.csv')
+data2 = pd.read_excel('~/Desktop/AmericanDream/Data/01_raw/2020_Data_Professional_Salary_Survey_Responses.xlsx')
+
+
+#print (data.head())
+
+print(data2.head())
