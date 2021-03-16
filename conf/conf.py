5b04c933827404244e27b227695be5817a60b385
fberrabah <farid.berrabah@gmail.com>
2021-03-16 14:39:46 +0100

    Add info in notebook

---
+++ conf/conf.py
@@ -0,0 +1,13 @@
+import mysql.connector
+
+mydb = mysql.connector.connect(
+  host="localhost",
+  user="FARIDZ",
+  password="Password08-",
+  database="Americabdd"
+)
+mycursor = mydb.cursor()
+
+
+mysql_pseudo = "FARIDZ"
+mysql_mdp = "Password08-"
--- src/d01_data/load_data.py
+++ src/d01_data/load_data.py
@@ -1,10 +1,18 @@
 import csv
 import pandas as pd
-
-data = pd.read_csv('~/Desktop/AmericanDream/Data/01_raw/DataAnalyst.csv')
-data2 = pd.read_excel('~/Desktop/AmericanDream/Data/01_raw/2020_Data_Professional_Salary_Survey_Responses.xlsx')
+import sys
 
 
-#print (data.head())
+sys.path.insert(0, "/home/apprenant/Desktop/AmericanDream/")
 
-print(data2.head())
+from src.d00_utils.mysql_utils import mysql_connect, save_to_mysql
+
+data = pd.read_csv('/home/apprenant/Desktop/AmericanDream/Data/01_raw/DataAnalyst.csv')
+data2 = pd.read_excel('/home/apprenant/Desktop/AmericanDream/Data/01_raw/2020_Data_Professional_Salary_Survey_Responses.xlsx', skiprows=3)
+connect = mysql_connect()
+    
+# Save the table in mysql database
+save_to_mysql(db_connect=connect,df_to_save=data,df_name='Analyst')
+save_to_mysql(db_connect=connect,df_to_save=data2,df_name='Salary')
+
+    
