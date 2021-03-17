fe04fe860559cc88212a559e72e6fefec6eb6935
fberrabah <farid.berrabah@gmail.com>
2021-03-17 15:00:21 +0100

    add comm

--- src/d01_data/load_data.py
+++ src/d01_data/load_data.py
@@ -13,6 +13,7 @@ data2 = pd.read_excel('/home/apprenant/Desktop/AmericanDream/Data/01_raw/2020_Da
 connect = mysql_connect()
     
 # Save the table in mysql database
+
 save_to_mysql(db_connect=connect,df_to_save=data,df_name='Analyst')
 save_to_mysql(db_connect=connect,df_to_save=data2,df_name='Salary')
 
